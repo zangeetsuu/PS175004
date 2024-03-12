42353475eb027eeb558c55a960c52adb7b5718d2
zangeetsuu <147048575+zangeetsuu@users.noreply.github.com>
2024-03-12 20:29:58 +0100

    Update main.c

--- lab3/funkcje/funkcje/main.c
+++ lab3/funkcje/funkcje/main.c
@@ -8,17 +8,9 @@ int main()
     printf("-----------------------\n");
     printf("%i \n", sumTwoNumbers(2,3));
     printf("-----------------------\n");
-    //printf("%f \n", calculateAbsoluteValue(4));
-    printf("-----------------------\n");
     calculateFactorial(10);
     printf("-----------------------\n");
     printf("%i \n", sumNumbers(10));
-    printf("-----------------------\n");
-    printf("%i \n", sumSquares(4));
-    printf("-----------------------\n");
-    printf("%i \n", calculatePowerOfTwo(4));
-    printf("-----------------------\n");
-    printf("%i \n", calculateSquareRootFloor(3));
     printf("-----------------------\n");
     countFunctionCalls();
     countFunctionCalls();
@@ -26,9 +18,11 @@ int main()
     printf("-----------------------\n");
     printf("%i \n", calculateFactorialRecursively(3));
     printf("-----------------------\n");
-    printf("%i \n", calculateFibonacciRecursively(10));
+    printf("%i %lf\n", calculateGeometricSequenceRecursively(5, 1.5));
     printf("-----------------------\n");
-    printf("%i \n", calculate13(3));
+    printf("%i \n", calculate15(3));
+    printf("-----------------------\n");
+    printf("%i %i\n", calculateGCD(2,5);
     return 0;
 }
 int sumTwoNumbers(int a,int b){
@@ -36,10 +30,7 @@ int sumTwoNumbers(int a,int b){
 
 }
 
-float calculateAbsoluteValue(float a){
-   return (a>=0)?(a):(-a);
 
-}
 
 int calculateFactorial(unsigned int a){
     int silnia=1;
@@ -57,29 +48,7 @@ int sumNumbers(unsigned int n){
     return suma;
 }
 
-int sumSquares(int n){
-    int sum=0;
-    for(int i=1;i<=n;i++){
-        sum+=i*i;
 
-    }
-    return sum;
-}
-
-int calculatePowerOfTwo(int n){
-    int pow=1;
-    for(int i=1;i<=n;i++){
-        pow*=2;
-    }
-    return pow;
-}
-int calculateSquareRootFloor(unsigned int n){
-    int i = 0;
-    while(i*i <= n) {
-        i++;
-    }
-    return i-1;
-}
 
 
 void countFunctionCalls(){
@@ -95,26 +64,26 @@ int calculateFactorialRecursively(unsigned int n){
     }
     return n*calculateFactorialRecursively(n-1);
 }
-int calculateFibonacciRecursively(unsigned int n){
-    if(n==0){
-        return 0;
-    }
-    if(n==1){
-        return 1;
-    }
-    return calculateFibonacciRecursively(n-1)+calculateFibonacciRecursively(n-2);
 
-}
-int calculateArithmeticSequenceRecursively(unsigned int n, int d){
-    if (n==1){
-        return 1;
+double calculateGeometricSequenceRecursively(int n, double d) {
+    if (n == 1) {
+        return 1; // Wyraz początkowy
+    } else {
+        return d * calculateGeometricSequenceRecursively(n - 1, d);
     }
-    return 1+calculateArithmeticSequenceRecursively(n-1,d);
-
 }
 
-int calculate13(unsigned int n){
-    n=1;
-    return 2*calculate13(n-1)+3;
 
+int calculate15(int n) {
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    return calculate15(n - 1) + 2 * calculate15(n - 2);
+}
+
+int calculateGCD(int n, int m) {
+    if (m == 0) {
+        return n; // NWD(n, 0) = n
+    } else {
+        return calculateGCD(m, n % m);
+    }
 }
