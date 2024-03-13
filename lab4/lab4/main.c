2406702ec307e0f46815882222bc3e388597c2a4
zangeetsuu <147048575+zangeetsuu@users.noreply.github.com>
2024-03-13 11:19:53 +0100

    zadanie dodatkowe

---
+++ lab4/lab4/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%i \n",w424(6,7));
+    return 0;
+}
+
+int w424(int a, int b){
+    if((a%7)>(b%7)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
